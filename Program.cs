5ff72b6c6b846ef3c3347b81e4c84a88355c51ce
mshatov <79084793+mshatov@users.noreply.github.com>
2022-03-04 13:42:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+string str = "x";
+
+int n = 9,
+    center = n / 2,
+    x = center;
+
+for (int i = 0; i < n; i++)
+{
+    if (i <= center)
+    {
+        Console.SetCursorPosition(x--, i);
+        Console.WriteLine(str);
+        if (i < center)
+        {
+            str += "xx";
+        }
+    }
+    else
+    {
+        str = str.Remove(str.Length - 2);
+        Console.SetCursorPosition(++x + 1, i);
+        Console.WriteLine(str);
+    }
+}
+Console.ReadKey();
